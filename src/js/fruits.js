bc1157e767fe4ac1904741c556a5a625e706b573
mrudenko <m.rudenko@zaleos.net>
2021-06-04 15:17:40 +0200

    improve fruits physics

--- src/js/fruits.js
+++ src/js/fruits.js
@@ -1,41 +1,59 @@
 import * as PIXI from 'pixi.js'
 import app from "./app";
 
-// holder to store the aliens
-const aliens = [];
-const totalDudes = 10;
+// holder to store the fruits
+const fruits = [];
+const totalDudes = 3;
+const g = 0.05;
 
-const initAliens = () => {
-    for (let i = 0; i < totalDudes; i++) {
-        // create a new Sprite that uses the image name that we just generated as its source
-        const dude = PIXI.Sprite.from('assets/pineapple.png');
+class Fruit {
+    get sprite() {
+        return this._sprite;
+    }
+    set sprite(sprite) {
+        this._sprite = sprite;
+    }
+    constructor() {
+        this._sprite = PIXI.Sprite.from('assets/pineapple.png');
+        this._velocity = 0;
 
         // set the anchor point so the texture is centered on the sprite
-        dude.anchor.set(0.5);
-
+        this._sprite.anchor.set(0.5);
         // set a random scale for the dude - no point them all being the same size!
-        dude.scale.set(0.05 + Math.random() * 0.1);
-
+        this._sprite.scale.set(0.05 + Math.random() * 0.1);
         // finally lets set the dude to be at a random position..
-        dude.x = Math.random() * app.screen.width;
-        dude.y = 0;
-
-        //dude.tint = Math.random() * 0xFFFFFF;
+        this._sprite.x = Math.random() * app.screen.width;
+        this._sprite.y = app.screen.height;
 
         // create some extra properties that will control movement :
         // create a random direction in radians. This is a number between 0 and PI*2 which is the equivalent of 0 - 360 degrees
-        dude.direction = Math.random() * Math.PI;
+        this._sprite.direction = Math.PI;
 
         // this number will be used to modify the direction of the dude over time
-        dude.turningSpeed = Math.random() - 0.8;
+        //dude.turningSpeed = Math.random() - 0.8;
+        this._sprite.turningSpeed = this._sprite.x >= app.screen.width / 2 ? Math.random() * 0.2 + 0.2 : Math.random() * 0.2 - 0.2;
 
         // create a random speed for the dude between 2 - 4
-        dude.speed = 2 + Math.random() * 2;
+        this._sprite.speed = 12 + Math.random() * 2;
+    }
 
-        // finally we push the dude into the aliens array so it it can be easily accessed later
-        aliens.push(dude);
+    tick() {
+        this._velocity += Math.sin(Math.PI * 2 + g) * 2;
 
-        app.stage.addChild(dude);
+        this.sprite.direction += this.sprite.turningSpeed * 0.01;
+        this.sprite.x += Math.sin(this.sprite.direction) * this.sprite.speed;
+        this.sprite.y += Math.cos(this.sprite.direction) * (this.sprite.speed - this._velocity);
+        this.sprite.rotation = -this.sprite.direction - Math.PI / 2;
+    }
+}
+
+const initAliens = () => {
+    for (let i = 0; i < totalDudes; i++) {
+        const fruit = new Fruit();
+        // finally we push the dude into the fruits array so it it can be easily accessed later
+        fruits.push(fruit);
+
+        app.stage.addChild(fruit.sprite);
     }
 }
 
@@ -49,25 +67,26 @@ const getDropDudesCycle = () => {
 
     return () => {
         // iterate through the dudes and update their position
-        for (let i = 0; i < aliens.length; i++) {
-            const dude = aliens[i];
-            dude.direction += dude.turningSpeed * 0.01;
-            dude.x += Math.sin(dude.direction) * dude.speed;
-            dude.y += Math.cos(dude.direction) * dude.speed;
-            dude.rotation = -dude.direction - Math.PI / 2;
+        for (let i = 0; i < fruits.length; i++) {
+            fruits[i].tick();
+            if(fruits[i].sprite.y > app.screen.height) {
+                fruits[i] = new Fruit();
+                app.stage.addChild(fruits[i].sprite);
+                fruits[i].tick();
+            }
 
             // wrap the dudes by testing their bounds...
-            if (dude.x < dudeBounds.x) {
-                dude.x += dudeBounds.width;
-            } else if (dude.x > dudeBounds.x + dudeBounds.width) {
-                dude.x -= dudeBounds.width;
-            }
-
-            if (dude.y < dudeBounds.y) {
-                dude.y += dudeBounds.height;
-            } else if (dude.y > dudeBounds.y + dudeBounds.height) {
-                dude.y -= dudeBounds.height;
-            }
+            //if (dude.x < dudeBounds.x) {
+            //    dude.x += dudeBounds.width;
+            //} else if (dude.x > dudeBounds.x + dudeBounds.width) {
+            //    dude.x -= dudeBounds.width;
+            //}
+//
+            //if (dude.y < dudeBounds.y) {
+            //    dude.y += dudeBounds.height;
+            //} else if (dude.y > dudeBounds.y + dudeBounds.height) {
+            //    dude.y -= dudeBounds.height;
+            //}
         }
     };
 }
--- src/js/gameLoop.js
+++ src/js/gameLoop.js
@@ -1,17 +1,9 @@
 import spriteLoader from "./assetsLoader";
 import app from "./app";
 
-const bunny = spriteLoader("assets/bunny.png")
-bunny.anchor.set(0.5);
-bunny.x = 160
-bunny.y = 160
-app.stage.addChild(bunny);
 
 document.body.appendChild(app.view);
-const cat = spriteLoader("assets/cat.png")
-app.stage.addChild(cat);
 
 export default function play(delta = 1){
-    bunny.rotation -= 0.01 * delta;
-    cat.x += 1 + delta;
+
 }
